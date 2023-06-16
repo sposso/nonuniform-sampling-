15d19394cd1444f6f313544f49102fb5e125e8ce
Santiago Posso <spo230@uky.edu>
2023-06-16 15:48:48 -0400

    change loc name

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python3 main.py \
-    --experiment_name "stride_8" \
-    --data_localization "$PROJECT/lsa273_uksr/breastcancer/data/final_data/loc.csv" \
+    --experiment_name "sweeps" \
+    --data_localization "$PROJECT/lsa273_uksr/breastcancer/data/raw_data/locations.csv" \
     --model-checkpoint-folder "$PROJECT/lsa273_uksr/breastcancer/runs/checkpoints" \
     --logs-folder "$PROJECT/lsa273_uksr/breastcancer/runs/logs" \
     --devices 4 \
